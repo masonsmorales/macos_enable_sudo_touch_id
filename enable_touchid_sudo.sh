7d222040afb2e6822226fc95ce522ff7245883cc
Mason Morales <camo87@gmail.com>
2026-01-08 10:36:54 -0800

    Refactor Touch ID sudo script for macOS

--- enable_touchid_sudo.sh
+++ enable_touchid_sudo.sh
@@ -1,68 +1,39 @@
 #!/bin/bash
 
+# Verify we're running on macOS
+if [[ "$(uname -s)" != "Darwin" ]]; then
+    echo "Error: This script is intended for macOS only."
+    exit 1
+fi
+
 echo_enabled() {
     echo "Touch ID has been successfully enabled for sudo."
 }
 
 echo_already_enabled() {
-    echo "Touch ID has already been enabled for sudo"
-}
-# Function to update file for macOS Ventura (13.x)
-update_file_ventura() {
-    local file="/etc/pam.d/sudo"
-    local line="auth       sufficient     pam_tid.so"
-
-    if ! grep -q "$line" "$file" 2>/dev/null; then
-        nl='
-'
-        sudo sed -i '' '/# sudo: auth account password session/a\
-'"$line\\${nl}" "$file"
-        if [ $? -eq 0 ]; then
-            echo_enabled
-        else
-            echo "Unable to enable Touch ID for sudo. Error adding line to $file."
-            exit 1
-        fi
-    else
-        echo_already_enabled
-    fi
+    echo "Touch ID has already been enabled for sudo."
 }
 
-# General function to update file for other versions
-update_file_sonoma() {
+update_sudo_local() {
     local file="/etc/pam.d/sudo_local"
     local line="auth       sufficient     pam_tid.so"
 
     if [ ! -f "$file" ]; then
         echo "$file does not exist. Creating file and adding line."
-        echo "$line" | sudo tee "$file" \
-        && echo_enabled
+        echo "$line" | sudo tee "$file" > /dev/null \
+            && echo_enabled
     elif ! grep -q "$line" "$file" 2>/dev/null; then
-        echo "$line" | sudo tee -a "$file" \
-        && echo_enabled
+        echo "$line" | sudo tee -a "$file" > /dev/null \
+            && echo_enabled
     else
-        echo "Line already exists in $file."
+        echo_already_enabled
         return
     fi
 
-    if [ $? -neq 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "Error adding line to $file."
         exit 1
     fi
 }
 
-# Function to check macOS version
-check_macos_version() {
-    local os_version
-    os_version=$(sw_vers -productVersion)
-    if [[ "$os_version" == 13.* ]]; then
-        update_file_ventura
-    elif [[ "$os_version" == 14.* ]]; then
-        update_file_sonoma
-    else
-        echo "This script is intended for macOS Ventura (13.*) and Sonoma (14.*)."
-    fi
-}
-
-# Run the script
-check_macos_version
+update_sudo_local
